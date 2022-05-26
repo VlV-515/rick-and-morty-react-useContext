14401d955a8b2d414c0d6764b795f489e60a7551
VlV <email@email.com>
2022-05-25 23:49:02 -0500

    Feat⚙️Consulta y pinta los card por default

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -2,14 +2,31 @@ import { useContext } from 'react'
 import { CharactersContext } from '../context/charactersContext'
 
 function Characters() {
-  const test = useContext(CharactersContext)
+  const { characters } = useContext(CharactersContext)
+  console.log(characters)
   return (
     <div className='row'>
-      <div className='col-4'>
-        {test}
-        <h2>Name</h2>
-        <p>Image</p>
-      </div>
+      {characters.map((character, index) => (
+        <div className='col-3' key={index}>
+          <div className='card mb-4'>
+            <img
+              className='card-img-top'
+              src={character.image}
+              alt={character.name}
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>{character.name}</h5>
+              <p className='card-text'>
+                <b>Species: </b>
+                {character.species}
+                <br />
+                <b>status: </b>
+                {character.status}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
--- src/context/charactersContext.jsx
+++ src/context/charactersContext.jsx
@@ -1,10 +1,25 @@
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import Axios from 'axios'
 
 export const CharactersContext = createContext()
 
 function CharactersContextProvider({ children }) {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://rickandmortyapi.com/api/character/').then((res) => {
+      if (res.status == 200) {
+        setCharacters(res.data.results)
+      }
+    })
+  }, [])
+
   return (
-    <CharactersContext.Provider value={'Rick and Morty'}>
+    <CharactersContext.Provider
+      value={{
+        characters,
+      }}
+    >
       {children}
     </CharactersContext.Provider>
   )
